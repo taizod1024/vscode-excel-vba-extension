3359135e2ccf5cc37903cb9a7767f8c2b8f46976
yamamoto.taizo <taizod1024@gmail.com>
2026-02-11 23:26:34 +0900

    fix: remove unnecessary blank line in activate function

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import { execPowerShell } from "./utils/execPowerShell";
 
 export function activate(context: vscode.ExtensionContext) {
   excelvba.activate(context);
-  
+
   // Run Install-Addin.ps1 on startup
   const installAddinPath = vscode.Uri.joinPath(context.extensionUri, "bin", "Install-Addin.ps1").fsPath;
   execPowerShell(installAddinPath, []);
