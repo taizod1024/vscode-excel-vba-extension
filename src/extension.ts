6b16f25a70c6508af5035084f9cfca6e36b6a8c4
yamamoto.taizo <taizod1024@gmail.com>
2026-02-11 23:26:27 +0900

    feat: add instructions to enable Excel add-in and content for proper functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 import * as vscode from "vscode";
 import { excelvba } from "./ExcelVba";
+import { execPowerShell } from "./utils/execPowerShell";
 
 export function activate(context: vscode.ExtensionContext) {
   excelvba.activate(context);
+  
+  // Run Install-Addin.ps1 on startup
+  const installAddinPath = vscode.Uri.joinPath(context.extensionUri, "bin", "Install-Addin.ps1").fsPath;
+  execPowerShell(installAddinPath, []);
 }
 
 export function deactivate() {
