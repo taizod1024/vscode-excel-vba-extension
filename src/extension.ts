e71c7af42d2070513faec6f2a57d06d7fdf523c3
yamamoto.taizo <taizod1024@gmail.com>
2026-02-12 00:28:06 +0900

    refactor: remove Install-Addin and Remove-Addin scripts from the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,8 @@
 import * as vscode from "vscode";
 import { excelvba } from "./ExcelVba";
-import { execPowerShell } from "./utils/execPowerShell";
 
 export function activate(context: vscode.ExtensionContext) {
   excelvba.activate(context);
-
-  // Run Install-Addin.ps1 on startup
-  const installAddinPath = vscode.Uri.joinPath(context.extensionUri, "bin", "Install-Addin.ps1").fsPath;
-  execPowerShell(installAddinPath, []);
 }
 
 export function deactivate() {
